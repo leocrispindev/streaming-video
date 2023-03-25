813fe7dc94df763c39d035ed97a19dda9e142785
lcrispin <lcrispin@uolinc.com>
2023-03-25 11:09:17 -0300

    Criando structura do admin-manager-api

---
+++ admin/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+C
+settings.gradle.kts,7/4/749edfcc96398253e5b3416184e95c46621da850
+@
+build.gradle.kts,d/b/dbcff70658daf80b53ce624f6adcaa529df5ed8d
+I
+src/main/kotlin/Server.kt,2/d/2d5c7be8c3b640d24523557aeb708a9dd50c242f
---
+++ admin/build.gradle.kts
@@ -0,0 +1,34 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.5.31"
+    application
+}
+
+group = "me.leonardo"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    jcenter()
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven")
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("io.ktor:ktor-server-netty:1.6.0")
+    implementation("io.ktor:ktor-html-builder:1.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.7.2")
+}
+
+tasks.test {
+    useJUnit()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "11"
+}
+
+application {
+    mainClass.set("ServerKt")
+}
