fa0a8e15741155815c78dc4bc3963c5d9fa5a895
Leonardo <leonardo.crispin.oliveira00@gmail.com>
2023-04-07 22:44:36 -0300

    Add informações do video na notificação

--- stream-go-commons/pkg/model/notify.go
+++ stream-go-commons/pkg/model/notify.go
@@ -1,9 +1,21 @@
 package model_commons
 
 type NotifyProccess struct {
-	ProccessID string
-	VideoName  string
-	Action     int
-	TopicName  string
-	VideoId    int
+	ProccessID   string
+	VideoName    string
+	Action       int
+	TopicName    string
+	VideoId      int
+	VideoOptions VideoOptions
+}
+
+type VideoOptions struct {
+	Width    int     // Width of frames.
+	Height   int     // Height of frames.
+	Depth    int     // Depth of frames.
+	Bitrate  int     // Bitrate for video encoding.
+	Frames   int     // Total number of frames.
+	Duration float64 // Duration of video in seconds.
+	Fps      float64 // Frames per second.
+	Codec    string
 }
