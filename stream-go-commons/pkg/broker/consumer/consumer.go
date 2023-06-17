5a66f31b43625681d0fdcd4272688daa4525ee0a
lcrispin <leonardo.crispin.oliveira00@gmail.com>
2023-06-17 16:54:00 -0300

    Upgrade consumer - groupID, partition and offset

--- stream-go-commons/pkg/broker/consumer/consumer.go
+++ stream-go-commons/pkg/broker/consumer/consumer.go
@@ -3,6 +3,7 @@ package consumer
 import (
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/Shopify/sarama"
 )
@@ -19,10 +20,10 @@ type Consumer struct {
 
 func (c *Consumer) ReadMessage(f func(<-chan *sarama.ConsumerMessage)) {
 
-	partition, err := c.ConsumerImpl.ConsumePartition(c.Topic, 0, sarama.OffsetOldest)
+	partition, err := c.ConsumerImpl.ConsumePartition(c.Topic, int32(c.Partition), sarama.OffsetOldest)
 
 	if err != nil {
-		log.Fatalf("Erro on Read Message %s", err.Error())
+		log.Fatalf("Error on Read Message %s", err.Error())
 
 	}
 
@@ -39,20 +40,24 @@ func getConsumerConfig() *sarama.Config {
 
 	consumerConfig.Consumer.Return.Errors = true
 	consumerConfig.Version = sarama.V2_2_0_0 // versão do Kafka
-
+	consumerConfig.Consumer.Offsets.Initial = sarama.OffsetOldest
+	consumerConfig.Consumer.Group.InstanceId = os.Getenv("KAFKA_GROUP_ID")
 	return consumerConfig
 }
 
-func CreateConsumer(groupID string, topics string) Consumer {
+func CreateConsumer(topics string) Consumer {
 
 	consumer, err := sarama.NewConsumer([]string{os.Getenv("KAFKA_HOST")}, getConsumerConfig())
 	if err != nil {
 		log.Fatalln("Erro ao criar consumidor:", err)
 	}
 
+	partition, _ := strconv.Atoi(os.Getenv("KAFKA_CONSUMER_PARTITION"))
+
 	c := Consumer{
 		ConsumerImpl: consumer,
 		Topic:        topics,
+		Partition:    partition,
 	}
 
 	return c
