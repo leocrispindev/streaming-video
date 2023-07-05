85cf7fd30f9691ffd5ec624338311fcc67a7c309
lcrispin <leonardo.crispin.oliveira00@gmail.com>
2023-07-04 23:11:56 -0300

    gradle files

---
+++ watcher-socket/build.gradle.kts
@@ -0,0 +1,33 @@
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+
+plugins {
+    kotlin("jvm") version "1.8.22"
+    id("io.ktor.plugin") version "2.3.2"
+}
+
+group = "com.br"
+version = "0.0.1"
+application {
+    mainClass.set(".com.br.ApplicationKt")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-websockets-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("com.google.code.gson:gson:2.7")
+    implementation("org.apache.kafka:kafka-clients:3.4.1")
+
+    testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
