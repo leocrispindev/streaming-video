720057b95b3f445555964544bd1e11b82dbc6e13
lcrispin <leonardo.crispin.oliveira00@gmail.com>
2023-07-30 00:43:04 -0300

    Add user session attr

--- watcher-socket/src/main/kotlin/com/br/watcher/model/Message.kt
+++ watcher-socket/src/main/kotlin/com/br/watcher/model/Message.kt
@@ -2,4 +2,4 @@ package com.br.watcher.model
 
 import org.apache.kafka.common.protocol.types.Field.Str
 
-data class Message(var videoName : String, var videoId : Int, var videoExtension : String)
+data class Message(val videoName : String, val videoId : Int, val videoExtension : String, val session : String)
