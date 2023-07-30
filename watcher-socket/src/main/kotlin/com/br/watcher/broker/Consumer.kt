7281e34c61177e868e4f9df345bd07f1586a7acf
lcrispin <leonardo.crispin.oliveira00@gmail.com>
2023-07-30 00:31:13 -0300

    Set consumer groupID

--- watcher-socket/src/main/kotlin/com/br/watcher/broker/Consumer.kt
+++ watcher-socket/src/main/kotlin/com/br/watcher/broker/Consumer.kt
@@ -3,6 +3,7 @@ package com.br.watcher.broker
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.consumer.ConsumerRecords
 import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.common.serialization.StringDeserializer
 import java.util.Properties
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.seconds
@@ -11,12 +12,13 @@ import kotlin.time.toJavaDuration
 class Consumer {
 
     private var consumer : KafkaConsumer<String, String>
-    constructor(topic : String) {
+    constructor(topic : String, groupID: String) {
 
         val prop = Properties().also {
-            it[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = ""
-            it[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = ""
-            it[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = ""
+            it[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = "http://localhost:9092"
+            it[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = "org.apache.kafka.common.serialization.StringDeserializer"
+            it[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = "org.apache.kafka.common.serialization.StringDeserializer"
+            it[ConsumerConfig.GROUP_ID_CONFIG] = groupID
         }
 
         consumer = KafkaConsumer(prop)
@@ -25,6 +27,7 @@ class Consumer {
     }
 
    fun StartPoll(time : Int) : ConsumerRecords<String, String> {
+
         return consumer.poll(time.seconds.toJavaDuration())
    }
 
