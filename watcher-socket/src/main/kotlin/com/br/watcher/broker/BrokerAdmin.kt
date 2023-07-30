1ea99f533d262b3c6f95f267d849829cba6f5807
lcrispin <leonardo.crispin.oliveira00@gmail.com>
2023-07-30 00:45:33 -0300

    Verify topic exist | create topic

--- watcher-socket/src/main/kotlin/com/br/watcher/broker/BrokerAdmin.kt
+++ watcher-socket/src/main/kotlin/com/br/watcher/broker/BrokerAdmin.kt
@@ -1,25 +1,43 @@
 package com.br.watcher.broker
 
-import com.typesafe.config.ConfigException.Null
-import org.apache.kafka.clients.admin.KafkaAdminClient
+import org.apache.kafka.clients.admin.AdminClient
+import org.apache.kafka.clients.admin.AdminClientConfig
+import org.apache.kafka.clients.admin.NewTopic
 import org.apache.kafka.common.KafkaException
+import java.util.*
+import kotlin.collections.ArrayList
 
 object BrokerAdmin {
 
     // TODO define admin properties
-    init {
+    lateinit var admin : AdminClient
 
+    init {
+        val prop = Properties().also {
+            it[AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG] = "http://localhost:9092";
+        }
+
+        admin = AdminClient.create(prop)
+        println("admin created");
     }
 
     // TODO create topic if not exists
-    fun checkTopic() {
+
+     fun createTopic(name: String) : Boolean {
         try {
+            val newTopic = NewTopic(name, 3, 0)
 
+            val listTopics = mutableListOf(newTopic)
+            val result = admin.createTopics(listTopics);
 
+            println("Created topic: " + result.topicId(name));
+            return true
 
         }catch (e : KafkaException) {
             throw e
         }
     }
-
+     fun topicExist(topicName : String) : Boolean {
+       return admin.listTopics().names().get().contains(topicName)
+    }
 }
